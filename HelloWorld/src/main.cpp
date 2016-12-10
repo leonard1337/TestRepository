bc5dbee31b9143f8ca81ca391e42e5fd52b72e23
leonard <leonard@alpha>
2016-12-10 17:02:59 +0100

    Initial commit.

---
+++ HelloWorld/src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Dec 10, 2016
+ *      Author: leonard
+ */
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+}
+
+
