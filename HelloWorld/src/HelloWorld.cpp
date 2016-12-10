fe30942e773c3bcd54876ecbff1f2182406f270b
leonard <leonard@alpha>
2016-12-10 17:56:34 +0100

    CHanged things.

--- HelloWorld/src/HelloWorld.cpp
+++ HelloWorld/src/HelloWorld.cpp
@@ -1,15 +1,7 @@
-//============================================================================
-// Name        : HelloWorld.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
 
 #include <iostream>
-using namespace std;
 
 int main() {
-	cout << "Hello World!" << endl; // prints Hello World!
+	std::cout << "Hello World!" << std::endl; // prints Hello World!
 	return 0;
 }
